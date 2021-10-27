a58c886e13617ea842cb298e50b49101394a601c
Yury Tkachyk <yury_tkachyk@epam.com>
2021-10-27 17:47:53 +0300

    chrome driver fix

--- src/test/java/com/epam/automation/test/PasteBinPageTest.java
+++ src/test/java/com/epam/automation/test/PasteBinPageTest.java
@@ -1,6 +1,7 @@
 package com.epam.automation.test;
 
 import com.epam.automation.page.PasteBinPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -28,6 +29,7 @@ public class PasteBinPageTest {
 
     @BeforeMethod
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         page = new PasteBinPage(driver);
     }
